6ad96330e44b5da45694bf301f78bd7bacb52bc7
Nicolas Buzzi <nico.cbuzzi@gmail.com>
2018-11-06 15:28:39 -0300

    Adding myapp.rb

---
+++ myapp.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'json'
+
+get '/' do
+    'Build Server is running.'
+end
+
+post '/event_handler' do
+    @payload = JSON.parse(params[:payload])
+  
+    case request.env['HTTP_X_GITHUB_EVENT']
+    when "pull_request"
+      if @payload["action"] == "opened"
+        process_pull_request(@payload["pull_request"])
+      end
+    end
+  end
+  
+  helpers do
+    def process_pull_request(pull_request)
+      puts "It's #{pull_request['title']}"
+    end
+  end
